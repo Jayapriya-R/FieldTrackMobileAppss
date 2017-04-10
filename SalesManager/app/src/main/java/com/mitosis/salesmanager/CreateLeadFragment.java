9636524979df2ed8c52fbfa4ae1e93b7182dd261
mitosis <vasanthipriya25@gmail.com>
2017-04-10 15:29:18 +0530

    name add 126 lin

--- SalesManager/app/src/main/java/com/mitosis/salesmanager/CreateLeadFragment.java
+++ SalesManager/app/src/main/java/com/mitosis/salesmanager/CreateLeadFragment.java
@@ -122,7 +122,7 @@ String clientname,desitination,address1,address2,cityy,statee,zipcodee,mphonee,s
                 spinnerselection();
             }
         });
-//Shivaramakeishnsn
+
         clientname=Clientname.getText().toString();
         desitination=Desitination.getText().toString();
         address1 =mAddress.getText().toString();
